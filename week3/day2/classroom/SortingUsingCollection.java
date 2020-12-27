7a2e863c4a00fcd3717ef126af94a8d7f4309ff9
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-12-27 17:38:49 +0530

    Update SortingUsingCollection.java

--- week3/day2/classroom/SortingUsingCollection.java
+++ week3/day2/classroom/SortingUsingCollection.java
@@ -4,6 +4,7 @@ public class SortingUsingCollection {
 
 	public static void main(String[] args) {
 
+		String[] input = {"HCL","Wipro","Aspire Systems","CTS"};
 //		Declare a String array and add the Strings values as (HCL, Wipro,  Aspire Systems, CTS)		
 
 //		get the length of the array		
